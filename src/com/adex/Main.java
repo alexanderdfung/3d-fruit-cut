dfdd10314d4b9de68a9550af9abe7056fd04b1bc
Alex Fung <alexfung018@gmail.com>
2020-01-14 10:46:34 -0800

    Added movement based on W, S key presses

--- src/com/adex/Main.java
+++ src/com/adex/Main.java
@@ -1,45 +1,59 @@
 package com.adex;
 
 import javafx.application.Application;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-// import javafx.scene.control.Label;
-// import javafx.scene.layout.StackPane;
-// import javafx.scene.shape.Sphere;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Box;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.paint.Color;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final double WIDTH = 1500.0;
+    private static final double HEIGHT = 800.0;
 
     @Override
     public void start(Stage stage) {
-        // Sphere sphere = new Sphere();
-        // sphere.setRadius(50.0);
-        // sphere.setTranslateX(200);
-        // sphere.setTranslateY(150);
-        // Group root = new Group(sphere);
-        // Scene scene = new Scene(root, 600, 300);
-        // stage.setTitle("Sphere object");
         Box box = new Box();
-        box.setWidth(200.0);
-        box.setHeight(400.0);
-        box.setDepth(200.0);
-        box.setTranslateX(300);
-        box.setTranslateY(300);
-        PhongMaterial textureMaterial = new PhongMaterial();
-        textureMaterial.setDiffuseColor(Color.RED);
-        box.setMaterial(textureMaterial);
-        Group root = new Group(box);
-        Scene scene = new Scene(root, 600, 600);
-        stage.setTitle("Drawing a Box");
+        box.setWidth(100.0);
+        box.setHeight(300.0);
+        box.setDepth(500.0);
 
-        // String javaVersion = System.getProperty("java.version");
-        // String javafxVersion = System.getProperty("javafx.version");
-        // Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Sphere sphere = new Sphere(150.0);
+
+        // Box shape = box; String title = "Moving Box";
+        Sphere shape = sphere; String title = "Moving Sphere";
+
+        PhongMaterial textureMaterial = new PhongMaterial();
+        textureMaterial.setDiffuseColor(Color.LIGHTBLUE);
+        shape.setMaterial(textureMaterial);
+
+        Group group = new Group();
+        group.getChildren().addAll(shape);
+
+        Scene scene = new Scene(group, WIDTH, HEIGHT);
+        scene.setFill(Color.WHITE);
+        scene.setCamera(new PerspectiveCamera());
+
+        shape.setTranslateX(WIDTH / 2.0);
+        shape.setTranslateY(HEIGHT / 2.0);
+
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case W:
+                    shape.setTranslateZ(shape.getTranslateZ() + 150.0);
+                    break;
+                case S:
+                    shape.setTranslateZ(shape.getTranslateZ() - 150.0);
+                    break;
+            }
+        });
+
         stage.setScene(scene);
+        stage.setTitle(title);
         stage.show();
     }
 
